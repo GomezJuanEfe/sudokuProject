1d42790476b8865f36722b70164370dd054c18d2
Juan Gomez <jf.gomezramirez@gmail.com>
2022-07-01 16:27:57 -0700

    HTML Structure

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Juan");
+/* Return the HTML elements of an specific sudoku index */
+function findRowsAndColums(inp) {
+  let ind = inp;
+  $('input').each(function(index, element) {
+    if ($(element).data('id').includes(ind)) {
+      console.log(element);
+    }
+  });
+}
+
+/* Return the HTML elements of an specific sudoku-block index */
+function findBlock(inp) {
+  let num = inp;
+  $('div .row > div').each(function (index, element) {
+     if ($(element).data('bl') == num) {
+        console.log(element);
+     }
+  });
+}
