08126abd97159c57e53f20dbf92b12050a9e9652
Akane Kano <bglakane@gmail.com>
2022-07-04 10:22:09 -0700

    modal part is done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /* Return the HTML elements of an specific sudoku index */
 function findRowsAndColums(inp) {
   let ind = inp;
-  $('input').each(function(index, element) {
+  $('input').each(function (index, element) {
     if ($(element).data('id').includes(ind)) {
       console.log(element);
     }
@@ -12,8 +12,41 @@ function findRowsAndColums(inp) {
 function findBlock(inp) {
   let num = inp;
   $('div .row > div').each(function (index, element) {
-     if ($(element).data('bl') == num) {
-        console.log(element);
-     }
+    if ($(element).data('bl') == num) {
+      console.log(element);
+    }
   });
 }
+
+// MODAL START
+$(function () {
+  let open = $('.modal-open'),
+    close = $('.modal-close'),
+    container = $('.modal-container');
+
+  //Clicking open modal button opens the modal.
+  open.on('click', function () {
+    container.addClass('active');
+    return false;
+  });
+
+  //Clicking "x" closes the modal.
+  close.on('click', function () {
+    container.removeClass('active');
+  });
+
+  //Clicking on the translucent background closes the modal.
+  $(document).on('click', function (e) {
+    if (!$(e.target).closest('.modal-body').length) {
+      container.removeClass('active');
+    }
+  });
+
+  //Clicking on try again button closes the modal.
+  $(document).on('click', function (e) {
+    if (!$(e.target).closest('try-again').length) {
+      container.removeClass('active');
+    }
+  });
+});
+// MODAL END
