be70a1d16aedb9257560ce81b1c448254b5c148d
Selin <selyyyyn@gmail.com>
2022-07-07 09:01:21 +0900

    check wrong function

--- index.js
+++ index.js
@@ -68,4 +68,19 @@ $('div.sudoku-container')
     compareRowsColumns(columnValues);
   })
 
-
+  function restriction() {
+    var e = event || window.event;  // get event object
+    var key = e.keyCode || e.which; // get key cross-browser
+  
+    if (key <= 48 || key > 57) { //if it is not a number ascii code
+        //Prevent default action, which is inserting character
+        if (e.preventDefault) e.preventDefault(); //normal browsers
+        e.returnValue = false; //IE
+    }
+  }
+ 
+  console.log($('input'));
+  $('input').each(function(index, element){
+      console.log($(element).hasClass("a1"));
+    
+  })
